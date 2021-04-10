07a2c30488772b7207c5161dbb343ef52294810b
Cameron St. Amant <cameronstamant@protonmail.com>
2021-04-10 16:45:36 -0600

    Add validation and sanitation to login process

--- controllers/wikiController.js
+++ controllers/wikiController.js
@@ -6,10 +6,34 @@ exports.login_get = (req, res, next) => {
   res.render('login');
 };
 
-exports.login_post = passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/login',
-});
+exports.login_post = [
+  body('email', 'Please enter your email.')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('password', 'Please enter your password.')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render('login', {
+        email: req.body.email,
+        password: req.body.password,
+        errors: errors.array(),
+      });
+      return;
+    }
+    next();
+  },
+
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+  }),
+];
 
 exports.signup_get = (req, res, next) => {
   res.render('signup');
