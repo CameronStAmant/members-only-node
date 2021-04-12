b8d3664878d43143d613284b78a1541d4b224c3f
Cameron St. Amant <cameronstamant@protonmail.com>
2021-04-12 11:43:45 -0600

    Add message to home page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const Message = require('../models/message');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express', user: req.user });
+  Message.find({}, 'title author body timestamp')
+    .populate('author')
+    .exec((err, list_messages) => {
+      if (err) {
+        return next(err);
+      }
+      console.log(list_messages);
+      res.render('index', {
+        title: 'Express',
+        user: req.user,
+        messages: list_messages,
+      });
+    });
 });
 
 module.exports = router;
