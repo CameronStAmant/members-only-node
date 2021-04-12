0d55df35c1bca41fa491cdec469c88519527290d
Cameron St. Amant <cameronstamant@protonmail.com>
2021-04-12 11:42:09 -0600

    Add luxon virtual propertry

--- models/message.js
+++ models/message.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
 
 const Schema = mongoose.Schema;
 
@@ -8,4 +9,9 @@ const MessageSchema = new Schema({
   body: { type: String, required: true },
   author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
 });
+
+MessageSchema.virtual('timestamp_formatted').get(function () {
+  return DateTime.fromJSDate(this.timestamp).toLocaleString(DateTime.DATE_MED);
+});
+
 module.exports = mongoose.model('Message', MessageSchema);
