3f33461dc2bfea0c82c9b4c434241ef32935a711
Cameron St. Amant <cameronstamant@protonmail.com>
2021-04-12 13:11:29 -0600

    Add admin form and functionality on home page

--- routes/index.js
+++ routes/index.js
@@ -10,13 +10,27 @@ router.get('/', (req, res, next) => {
       if (err) {
         return next(err);
       }
-      console.log(list_messages);
       res.render('index', {
         title: 'Express',
         user: req.user,
         messages: list_messages,
       });
     });
+});
+
+router.post('/', async (req, res, next) => {
+  const message = await Message.findById(req.body.id).exec();
+  if (message === null) {
+    let err = new Error('Message not found');
+    err.status = 404;
+    return next(err);
+  }
+  Message.findByIdAndDelete(req.body.id, (err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/');
+  });
 });
 
 module.exports = router;
